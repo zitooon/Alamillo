f7912c00daa156f33a1dc0186d305f63db52ce40
zitooon <olivier.derobert@gmail.com>
2011-02-26 19:10:05 +0100

    adding bundler

--- config/environment.rb
+++ config/environment.rb
@@ -15,8 +15,6 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'haml'  
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
